aaabce11a1d7a90440d05f82e0c289b3d19b40b2
Rohit <puthenrohit@gmail.com>
2020-04-08 12:08:58 +0530

    Check if the returned data is PDF

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # covidfyi_find404
 
 
+## Install necessary modules 
+
+``` npm install ```
+
 ## Run the script as a worker to hit the list of URLs in a set interval
 
 ``` node app.js ```
--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const instance = axios.create({
   })
 });
 
-instance.get('https://icmr.nic.in/sites/default/files/whats_new/ICMR_testing_update_07April_9PM_IST.pdf').then(({data})=>{
-	console.log(data);
+instance.get('https://icmr.nic.in/sites/default/files/whats_new/ICMR_testing_update_07April_9PM_IST.pdf').then((data)=>{
+	//Buffer data
+	console.log(data.data);
 	//Link works. Can do additional file mime type check if you want to verify if the link is a PDF
+	//To check if returned data type is PDF
+	if(data.headers['content-type'] === 'application/pdf'){
+		//file type is PDF
+		console.log('Is PDF');
+	}
 }).catch(({response})=>{
 	if(response.status === 404){
 		console.log('link expired');
