d8cbd61c1498b5d3b56464bad4f9d76071ce6893
Rohit <puthenrohit@gmail.com>
2020-04-08 12:20:04 +0530

    Destructed HTTPS module, only used Agent method

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
-const https = require('https');
+const {Agent} = require('https');
 
 const instance = axios.create({
-  httpsAgent: new https.Agent({  
+  httpsAgent: new Agent({  
     rejectUnauthorized: false
   })
 });
