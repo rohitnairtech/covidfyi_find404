7ee0428939c677f56bf4667e747ec073d9ae99b6
Rohit <puthenrohit@gmail.com>
2020-04-08 20:30:26 +0530

    Destructed axios

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const {Agent} = require('https');
 
-const instance = axios.create({
+const {get} = axios.create({
   httpsAgent: new Agent({  
     rejectUnauthorized: false
   })
 });
 
-instance.get('https://icmr.nic.in/sites/default/files/whats_new/ICMR_testing_update_07April_9PM_IST.pdf').then((data)=>{
+get('https://icmr.nic.in/sites/default/files/whats_new/ICMR_testing_update_07April_9PM_IST.pdf').then((data)=>{
 	//Buffer data
 	console.log(data.data);
 	//Link works. Can do additional file mime type check if you want to verify if the link is a PDF
